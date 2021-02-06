ba41fccdb693ac4a824d61999ff1611c47f67b88
Zachary Munson <zmunson85@gmail.com>
2021-02-06 15:53:03 -0700

    MultiTable Initial Commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using static System.Console;
+
+namespace MultiTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplicationtable = new int[10, 10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    multiplicationtable[i, j] = (i + 1) * (j + 1);
+                }
+
+            }
+
+            WriteLine();
+        
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Write(multiplicationtable[i,j]+ "\t");
+                }
+                WriteLine();
+
+            }
+            WriteLine();
+        }
+    }
+}
+
+
+
+//     int[,] multiplicationTable = new int[10, 10];
+//     for (int i = 0; i < 10; i++)
+//     {
+//         for (int j = 0; j < 10; j++)
+//         {
+//             multiplicationTable[i, j] = (i + 1) * (j + 1);
+//         }
+//     }
+//     WriteLine();
+//     for (int i = 0; i < 10; i++)
+//     {
+//         for (int j = 0; j < 10; j++)
+//         {
+//             Write(multiplicationTable[i, j] + "\t");
+//         }
+
+//         WriteLine();
+
+//     }
+//     WriteLine();
